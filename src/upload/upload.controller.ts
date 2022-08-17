5b449dc5ebc2ac226ce74820a989ea220dc45ed9
waiyan <waiyanavionics@gmail.com>
2022-08-17 09:36:10 +0630

    init

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { CreateUploadDto } from './dto/create-upload.dto';
+import { UpdateUploadDto } from './dto/update-upload.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(AnyFilesInterceptor())
+  create(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files)
+    return files
+  }
+
+  @Get()
+  findAll() {
+    return this.uploadService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.uploadService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUploadDto: UpdateUploadDto) {
+    return this.uploadService.update(+id, updateUploadDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.uploadService.remove(+id);
+  }
+}
